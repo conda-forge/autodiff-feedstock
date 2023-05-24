9d3e18de2962ecd65539ec940bad0b10b5b226cc
briha <benno.riha@icloud.com>
2023-05-24 08:14:20 +0200

    using 10.14 for non-arm macOS, not testing when cross-compiling

--- recipe/build.sh
+++ recipe/build.sh
@@ -11,10 +11,19 @@ echo "Using $JOBS parallel jobs out of $((CPU_COUNT*2)) available to build autod
 mkdir .build
 cd .build
 
+# Turn of tests for 
+if [[ "${CONDA_BUILD_CROSS_COMPILATION:-}" == "1" ]]; then
+  build_tests="OFF"
+else
+  build_tests="ON"
+fi
+
+
 # Configure the build of autodiff
 cmake -GNinja .. ${CMAKE_ARGS} \
     -DCMAKE_BUILD_TYPE=Release     \
     -DAUTODIFF_BUILD_EXAMPLES=OFF  \
+    -DAUTODIFF_BUILD_TESTS=${build_tests}  \
     -DPYTHON_EXECUTABLE=$PYTHON
 
 # Build and install autodiff in $PREFIX
